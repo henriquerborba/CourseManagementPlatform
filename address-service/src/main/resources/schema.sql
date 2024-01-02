79a5f7721a01c12a281ac8e2a5314058ce456bb1
Henrique Borba <henrique.borba@ecomp.ufsm.br>
2024-01-01 23:40:45 -0300

    feat: database configuration

---
+++ address-service/src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS address;
+
+CREATE TABLE
+    IF NOT EXISTS address (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        street VARCHAR(100) NOT NULL,
+        number VARCHAR(100) NOT NULL,
+        city VARCHAR(100) NOT NULL,
+        state VARCHAR(100) NOT NULL,
+        country VARCHAR(100) NOT NULL,
+        zip_code VARCHAR(100) NOT NULL user_id UUID NOT NULL
+    );
